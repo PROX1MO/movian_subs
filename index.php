2f7cc647308c8197db59fc0ef49cfd2f2132e02f
PROXIMO <17911448+PROX1MO@users.noreply.github.com>
2020-03-16 10:30:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ else
 	$episode = isset($_POST['e']) ? $_POST['e'] : '';
 
 	if(empty($provider))
-		die('Error 403\n');
+		die('<html><head><title>404 Not Found</title></head><body bgcolor="white"><center><h1>404 Not Found</h1></center><hr><center>nginx/1.6.2</center></body></html>');
 
 	$bgsubs->setProvider($provider);
 
